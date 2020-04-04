ec6e07d58c94f1fc4c6376f73193a5a523dca987
Mark Olbert <mark@arcabama.com>
2020-04-04 15:23:42 -0700

    Created a test project to illustrate potential issue involving how help flags are processed

---
+++ HelpTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+
+namespace HelpTest
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var option = new Option<int>(
+                new[] { "-p", "--population" },
+                getDefaultValue: () => 1000,
+                description: "Number of people to simulate" );
+
+            option.Argument.AddValidator(
+                x => x.GetValueOrDefault<int>() <= 0 ? "Population must be >= 1" : null );
+
+            var _rootCommand = new RootCommand()
+            {
+                option
+            };
+
+            var population = -1;
+
+            _rootCommand.Handler = CommandHandler.Create( ( int p ) =>
+            {
+                population = p;
+            } );
+
+            var parseResult = _rootCommand.Invoke( "-h" );
+
+            Console.WriteLine( population < 0
+                ? "population argument not parsed"
+                : $"population argument parsed ({population})" );
+        }
+    }
+}
